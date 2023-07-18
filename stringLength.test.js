62f4af53cd80919a689c9e7d09e48e66a0f1b249
wineshuga <uzochukwuwinnie@gmail.com>
2023-07-18 21:49:58 +0100

    Use AAA pattern

--- stringLength.test.js
+++ stringLength.test.js
@@ -1,18 +1,42 @@
 const stringLength = require('./stringLength');
 
-// Arrange
 test('counts each character of string input', () => {
-    expect(stringLength("hello")).toBe(5);
+    // Arrange
+    const string = "hello"
+
+    // Act
+    const count = stringLength(string)
+
+    // Assert
+    expect(count).toBe(5);
 });
 
 test('at least 1 character long', () => {
-    expect(stringLength("helo")).toBe(4);
+    // Arrange
+    const string = "helo"
+
+    // Act
+    const count = stringLength(string)
+
+    // Assert
+    expect(count).toBe(4);
 });
 
 test('less than 10 character count', () => {
-    expect(stringLength("hello dear")).toBe(10);
+    // Arrange
+    const string = "hello dear"
+
+    // Act
+    const count = stringLength(string)
+
+    // Assert
+    expect(count).toBe(10);
 });
 
 test('throw error message when string is greater than 10', () => {
-    expect(() => stringLength("my name is winnie")).toThrow(Error);
+    // Arrange
+    const string = "my name is winnie"
+
+    // Act & Assert
+    expect(() => stringLength(string)).toThrow(Error);
 });
